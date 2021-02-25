2ea031e8e7c3a16099558f3801e4b5d5b31330ba
Wesley <wes.beck21@outlook.com>
2021-02-25 12:32:35 -0800

    commit tree c74703bd73d59b3a2e513d9d1e7e557374059b08

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 =  2;
+const num2 =  31;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max(5, 10, 15, 20)
